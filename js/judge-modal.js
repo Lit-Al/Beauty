db9ee9b6fb02682e346386e24a3c134a2781afa2
Litvinov Alexandr <zmeelov761rus@gmail.com>
2023-07-20 22:45:30 +0300

    commit lol

--- js/judge-modal.js
+++ js/judge-modal.js
@@ -1,10 +1,33 @@
-// Modal
-
 const closeBtn = document.querySelector('.close-judge-modal');
 const modal = document.querySelector('.judge-modal');
 const form = document.querySelector('.evaluations-form');
 const blur = document.querySelector('.blur');
 
+const radio = document.getElementsByClassName('evulation-radio')
+const balls = document.getElementById('balls')
+const ballsModal = document.getElementById('ballsModal')
+let numberBalls = 0;
+
+let arr = [];
+
+function checkType(node) {
+  if (!arr.includes(node.name)) {
+  numberBalls += + node.value
+  }
+  else {
+    node.disabled = true
+  }
+  console.log(!arr.includes(node.name));
+  arr.push(node.name)
+  for (const i of radio) {
+    if (i.name === node.name)
+      i.disabled = true
+  }
+  console.log(arr);
+  balls.innerHTML = numberBalls
+  ballsModal.innerHTML = numberBalls
+
+}
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
